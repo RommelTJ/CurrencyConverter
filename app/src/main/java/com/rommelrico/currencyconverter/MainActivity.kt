c79e2b402d94e43e8996d55c2fa0719b0210e678
Rommel Rico <me@rommelrico.com>
2018-10-23 10:54:49 -0700

    convertCurrency function

--- app/src/main/java/com/rommelrico/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/rommelrico/currencyconverter/MainActivity.kt
@@ -2,8 +2,14 @@ package com.rommelrico.currencyconverter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    fun convertCurrency(view: View) {
+        Log.i("Info", "Button pressed!")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
