0f3b7e3845ce1f87ce1559c034aae03c92ea4040
Rommel Rico <me@rommelrico.com>
2018-10-23 11:14:33 -0700

    Formatting to Currency

--- app/src/main/java/com/rommelrico/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/rommelrico/currencyconverter/MainActivity.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     fun convertCurrency(view: View) {
         val editText = findViewById<EditText>(R.id.currencyText)
+
         val amountInDlls: String = editText.text.toString()
         val conversionAmount = 18
         val convertedAmount = amountInDlls.toDouble() * conversionAmount
+
+        val format = NumberFormat.getCurrencyInstance(Locale.US)
+        val formattedAmnt = format.format(convertedAmount)
+        
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
